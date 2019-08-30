5b97778c201a5747e045509b4b7a2b7a6a9744b1
ndbellew <ndbellew@gmail.com>
2019-08-30 14:48:22 -0600

    weird linking error caused it to crash.

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-<?php echo "hello" ?>
+<!doctype html>
+<?php $title = "yee"; ?>
+<html>
+  <head>
+    <title>
+      <?php
+      echo $title;
+      ?>
+    </title>
+  </head>
+  <?php
+    $name = "Nathan Bellew";
+
+    echo "Welcome, our name is $name\nHow can we service you in this unholy hour?"
+
+    $items = array(
+      "alpha","beta", "gamma","kirby"
+    );
+    foreach ($items as $item){
+      echo "<li>$item</li>\n";
+    };
+  ?>
+
+
+</html>
