1b03d1c7ce7a9645ac07c27f92f69866500a3026
ndbellew <ndbellew@gmail.com>
2019-08-30 14:28:02 -0600

    php addition NOTE: all variables must use $ this is good and bad much like the self. denotation from python or the $ use in bash. however it is only for variables and nothing else.

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-<?php
+<!doctype html>
+<?php $title = "Gay Conversion Therapy"; ?>
+<html>
+  <head>
+    <title>
+      <?php
+      echo $title;
+      ?>
+    </title>
+  </head>
+  <?php
+    $name = "Nathan Bellew";
 
-echo "hello world";
+    echo "Welcome, our name is $name\nHow can we service you in this unholy hour?"
+  ?>
 
+
+</html>
