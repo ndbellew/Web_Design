a0b1e4e4347adbf76f25634b74d5c1a46dcc3b2f
ndbellew <ndbellew@gmail.com>
2019-08-30 14:51:34 -0600

    maybe this time

--- index.php
+++ index.php
@@ -8,18 +8,7 @@
       ?>
     </title>
   </head>
-  <?php
-    $name = "Nathan Bellew";
 
-    echo "Welcome, our name is $name\nHow can we service you in this unholy hour?"
-
-    $items = array(
-      "alpha","beta", "gamma","kirby"
-    );
-    foreach ($items as $item){
-      echo "<li>$item</li>\n";
-    };
-  ?>
 
 
 </html>
