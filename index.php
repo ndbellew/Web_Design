5db5216ebbe4463078800a2e0ad8fb313ea537ac
ndbellew <ndbellew@gmail.com>
2019-08-30 14:29:57 -0600

    added looping structure foreach works similar to foreach in c#

--- index.php
+++ index.php
@@ -12,6 +12,13 @@
     $name = "Nathan Bellew";
 
     echo "Welcome, our name is $name\nHow can we service you in this unholy hour?"
+
+    $items = array(
+      "alpha","beta", "gamma","kirby"
+    );
+    foreach ($items as $item){
+      echo "<li>$item</li>\n";
+    }
   ?>
 
 
