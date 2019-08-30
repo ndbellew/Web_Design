857636ad08f3a2bec7c1e615774d03a60d9978ba
ndbellew <ndbellew@gmail.com>
2019-08-30 14:54:40 -0600

    perhaps

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <!doctype html>
-<?php $title = "yee"; ?>
 <html>
   <head>
     <title>
-      <?php
-      echo $title;
-      ?>
     </title>
   </head>
-
-
-
 </html>
+
+  <?php
+    $name = "Nathan Bellew";
+
+    echo "Welcome, our name is $name\nHow can we service you in this unholy hour?"
+
+    $items = array(
+      "alpha","beta", "gamma","kirby"
+    );
+    foreach ($items as $item){
+      echo "<li>$item</li>\n";
+    };
+  ?>
