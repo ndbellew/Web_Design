e3345d788a7a81e8e2535a3fa3d9faf85c8274f7
ndbellew <ndbellew@gmail.com>
2019-08-30 21:07:55 -0600

    heroku didnt like that one

---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+  <head>
+    <title>
+    </title>
+    <?php
+    echo "<p>Created as of" . date("Y") . " \nAuthor: Nathan Bellew\nCompany: Dagglertopia &copy </p>";
+    ?>
+  </head>
+</html>
