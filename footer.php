ad40f6ae57aa1eb6db9c53d7a085802a9d86ba82
ndbellew <ndbellew@gmail.com>
2019-09-06 14:19:34 -0600

    Moved Flask App; Remade index My self projects should be seperate from school work especially if im using heroku.

--- footer.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "<p>Created as of" . date("Y") . " \nAuthor: Nathan Bellew\nCompany: Dagglertopia &copy </p>";
-?>
--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<!doctype html>
-<html>
-  <head>
-    <title>
-    </title>
-    <?php
-    echo "<p>Created as of" . date("Y") . " \nAuthor: Nathan Bellew\nCompany: Dagglertopia &copy </p>";
-    ?>
-  </head>
-</html>
+<?php
+ echo "Hello World\n";
+?>
