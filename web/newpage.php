001c2d23b3c04c790f2a7f332e00c7d1989c2c69
ndbellew <ndbellew@gmail.com>
2019-09-06 14:34:18 -0600

    session test 1 session should allow the variables to persist on every reload.

--- web/newpage.php
+++ web/newpage.php
@@ -1,3 +1,4 @@
 <?php
  echo "Hello World\n Im a proper redirect!\n";
+ $my_array = array("Alpha", "Beta", "Gamma");
 ?>
---
+++ web/state.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+$_SESSION["count"]=$_SESSION["count"]+1;
+echo "count=$count\n"
+ ?>
