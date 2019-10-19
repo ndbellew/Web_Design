11f2fbd991429470bb66672184d5459fc125b42e
Nathan Bellew <ndbellew@mavs.coloradomesa.edu>
2019-10-19 16:35:25 -0700

    Attempting to connect to db

---
+++ PHP/web/system.php
@@ -0,0 +1,22 @@
+<?php
+$params = parse_ini_file ('../database.ini');
+if ($params === false) {
+  throw new \Exception("Error reading database config file");
+}
+$dbhost=$params['host'];
+$dbport=$params['port'];
+$dbname=$params['database'];
+$dbuser=$params['user'];
+$dbpassword=$params['password'];
+$conStr = sprintf("pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
+                $dbhost,
+                $dbport,
+                $dbname,
+                $dbuser,
+                $dbpassword);
+try{
+  $conn = new PDO($conStr);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+  die("Error: Could not connect. " . $e->getMessage());
+}
